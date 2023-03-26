d999542878af60cb48158653f02a3d797b64b690
ViktorOsu <123021878+ViktorOsu@users.noreply.github.com>
2023-03-26 12:13:08 +0300

    initial commit

--- js/task-07.js
+++
@@ -1,9 +0,0 @@
-const fontSizeControl = document.querySelector('#font-size-control');
-const text = document.querySelector('#text');
-
-fontSizeControl.addEventListener('input', e => {
-    text.style.fontSize = `${e.target.value}px`;
-});
-
-
-
